bc291da2820e027f85590fbcf0d4c37f8011f953
Gustavo <gustavo.prog40@gmail.com>
2022-04-08 20:45:12 -0300

    asdfadfasdf

--- src/Controllers/postController.ts
+++ src/Controllers/postController.ts
@@ -15,9 +15,9 @@ var resultado = ''
 async function register(req: Request, res: Response) {
   try {
     cloudinary.config({
-      cloud_name: process.env.CLOUD_NAME,
-      api_key: process.env.API_KEY,
-      api_secret: process.env.API_SECRET,
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
     })
 
     cloudinary.uploader.upload(req.file?.path, function (result: any, error: any) {
